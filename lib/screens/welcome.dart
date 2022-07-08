51fcdd9d5601d328d00a984e80eeda43c64ba46c
nakkarintby <nakkarintby@hotmail.com>
2022-07-08 17:46:54 +0700

    ver2

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -52,7 +52,7 @@ class _WelcomePageState extends State<WelcomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('a'),
+        title: Text('as'),
       ),
       body: Center(
           // Center is a layout widget. It takes a single child and positions it
