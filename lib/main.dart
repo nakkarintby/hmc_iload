d03c729a53d8f7ae744e7ee800b861e6258e442c
nakkarintby <nakkarintby@hotmail.com>
2022-07-14 16:57:57 +0700

    ฟ

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'RTLS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
